d2bd9b7b137403ea96c0e06716aefa9175b6ba2d
arindam017 <arindam5961@gmail.com>
2019-06-05 19:31:06 +0530

    parser added

---
+++ parser_files/1.sh
@@ -0,0 +1,3 @@
+
+./csv_generator.py /home/arindam/Desktop/Sniper/sniper-6.1/expt_results/LRU_30_2
+
---
+++ parser_files/caller_ultimate_parser_single_core.sh
@@ -0,0 +1,46 @@
+export SNIPER_ROOT=/home/abhijit/new_sniper_61_damlp/sniper_61_damlp
+
+path1="/home/abhijit/sniper_original/sniper-6.1/results/DAAIP/"
+path2="/home/abhijit/sniper_original/sniper-6.1/results/LRU/"
+path3="/home/abhijit/sniper_original/sniper-6.1/results/SRRIP/"
+path4="/home/abhijit/sniper_original/sniper-6.1/results/REUSE_DEC_AFTER_8/"
+path5="/home/abhijit/sniper_original/sniper-6.1/results/REUSE_DEC_ON_EVICT/"
+
+parser="./parser_all_qsize.py"
+#All BENCHMARKS = 24
+#benchmarks=(astar libq lbm bwaves bzip cactusADM gcc gobmk GemsFDTD gromacs h264ref hmmer leslie milc namd omnetpp sjeng soplex sphinx tonto wrf zeusmp mcf perlbench)
+
+#REUSE SELECTED BENCHMARKS = 15
+#benchmarks=(astar bwaves bzip cactusADM gcc GemsFDTD lbm leslie libq mcf milc omnetpp soplex sphinx)
+benchmarks=(bzip cactusADM gcc lbm libq milc soplex)
+
+
+#Thrashing benchmarks = 8 milc to be added.
+#benchmarks=(cactusADM lbm leslie libq mcf sphinx milc bwaves)
+
+#REMAINING REUSE BENCHMARKS = 8
+#benchmarks=(gobmk gromacs h264ref namd perlbench sjeng tonto wrf zeusmp)
+
+
+#FOR RUNNING SINGLE BENCHMARK 
+#benchmarks=(bzip)
+
+#suffix=(1 10 20 100)
+#benchmarks=(cactusADM mcf sphinx milc astar leslie lbm bwaves libq)
+#benchmarks=(cactusADM sphinx)
+for i in {0..6}
+    do
+	echo "===================================================================================================================================="
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	#/home/abhijit/parser_files/ultimate_parser_single_core.py /home/abhijit/sniper_original/sniper-6.1_experimental/results/RECENTLY_INSERTED_AND_REUSED_3/${benchmarks[i]} 1
+	/home/arindam/Desktop/Sniper/sniper-6.1/parser_files/ultimate_parser_single_core.py /home/arindam/Desktop/Sniper/sniper-6.1/expt_results/LRU_30_2/${benchmarks[i]} 1
+	echo ""
+	echo ""
+	echo ""
+	echo ""
+	echo "===================================================================================================================================="
+       
+    done
